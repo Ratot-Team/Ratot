f87c4bd14815bd14fdec0bd6faa9af951671790e
IIIRataxIII <56645921+IIIRataxIII@users.noreply.github.com>
2020-09-23 17:59:49 +0100

    V0.2.1
    Pequenos melhoramentos

--- main.js
+++ main.js
@@ -112,11 +112,12 @@ client.on('message', message=>{
                 }
             break;
             case 'abraçar':
-                console.log(args[1]);
-                console.log(args[1].charAt(1));
                 if(!args[1]|| args[1].charAt(1)!='@')
-                    return message.reply('Menciona quem queres abraçar, por exemplo "$abraçar @Rat.exe Bot#1386"');
-                message.channel.send('Abraçei o ' + args[1] + ' a pedido do <@!' + message.author.id + '>');
+                    return message.reply('Menciona quem queres abraçar, por exemplo "$abraçar <@!756290869900083272>"');
+                if(args[1]=='<@!756290869900083272>')
+                    message.channel.send('Abraçei-me a mim mesmo a pedido do <@!' + message.author.id + '>');
+                else
+                    message.channel.send('Abraçei o ' + args[1] + ' a pedido do <@!' + message.author.id + '>');
                 break;
             case 'bot':
                 if(args[1]!='ping')
