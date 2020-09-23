87e549d7078272be7b8043d9b96ced5c7050211e
IIIRataxIII <56645921+IIIRataxIII@users.noreply.github.com>
2020-09-23 17:28:01 +0100

    V0.2.0
    Novidades!
    ->A linguagem do bot agora é em portugês.
    ->Foram acrescentados mais três comandos
    ->->$abraçar - O Bot abraça alguém que seja mencionado
    ->->bot ping - O Bot diz o valor do ping dele
    ->->meu ping - O bot diz o valor do teu ping
    ->O comando de $ping foi alterado e agora depois de várias vezes repetido continua responder com uma mensagem de muito mau humor, mas também dá o ping dele e o teu.

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const token = 'InsertYourBotTokenHere';
+const token = 'Insere aqui o token do bot';
 
 const prefix = '$';
 
 var lastPing, pingCounter;
 
 client.once('ready', ()=> {
-    console.log('Rat.exe Bot is online!');
+    console.log('Rat.exe Bot está online!');
     client.user.setActivity('$help', {type:'PLAYING'}).catch(console.error);
 });
 
@@ -18,87 +18,125 @@ client.on('message', message=>{
     let args = message.content;
     var isCommand = args.charAt(0) == '$';
     args= args.substring(prefix.length).split(" ");
-    
-    switch(args[0]){
-        case 'ping':
-            if (lastPing == message.author.id)
-                {
-                    pingCounter++;
-                    if(pingCounter>=5)
+    if(isCommand)
+    {
+        switch(args[0]){
+            case 'ping':
+                if (lastPing == message.author.id)
                     {
-                        message.reply('PONG MOTHERFUCKER! Aren\'t you tired of this shit?!');
-                        lastPing = message.author.id;
-                    }
-                    else
-                        message.reply('Pong!');
-                }
-            else
-                {
-                    lastPing = message.author.id;
-                    pingCounter = 1;
-                    message.reply('Pong!');
-                };
-            break;
-        case 'delete':
-            if(args[1]!='messages') message.reply('Did you mean "$delete messages"?');
-            else
-                {
-                    if(!args[2] || !Number.isInteger(parseInt(args[2]))) message.reply('Chose the number of messages to delete, for example "$delete messages 2"');
-                    else 
-                    {
-                        if(message.member.hasPermission('ADMINISTRATOR'))
-                            {
-                                if(args[2]>99)
-                                    return message.reply('You can only delete 99 messages!');
-                                var n = args[2];
-                                n++;
-                                message.channel.bulkDelete(n);
-                                message.reply(args[2] + ' messages deleted!').then(message=>{
-                                    message.delete({timeout:3000})
-                                })
-                                .catch(console.error);
-                            }
+                        pingCounter++;
+                        if(pingCounter>=5)
+                        {
+                            message.channel.send("FODASSE").then(m =>{
+                                var latency = m.createdTimestamp - message.createdTimestamp;
+                                var botPing = client.ws.ping;
+                                m.edit('PONG CARALHO! O meu ping é ' + botPing + 'ms e o teu é ' + latency + 'ms seu merdas!');
+                            });
+                            lastPing = message.author.id;
+                        }
                         else
-                            message.reply('Only Admins can delete messages!');
+                            message.reply('Pong!');
+                    }
+                else
+                    {
+                        lastPing = message.author.id;
+                        pingCounter = 1;
+                        message.reply('Pong!');
+                    };
+                break;
+            case 'delete':
+                if(args[1]!='messages') message.reply('Será que querias dizer "$delete messages"?');
+                else
+                    {
+                        if(!args[2] || !Number.isInteger(parseInt(args[2]))) message.reply('Escreve o número de mensagens que queres eliminar, por exemplo "$delete messages 2"');
+                        else 
+                        {
+                            if(message.member.hasPermission('ADMINISTRATOR'))
+                                {
+                                    if(args[2]>99)
+                                        return message.reply('Só podes eliminar 99 mensagens!');
+                                    if(args[2]<0)
+                                        return message.reply('Achas que podes elminar mensagens negativas? Pff humanos...');
+                                    var n = args[2];
+                                    n++;
+                                    message.channel.bulkDelete(n);
+                                    if(args[2]==1)
+                                         message.reply(args[2] + ' mensagem foi eliminada!').then(message=>{
+                                             message.delete({timeout:3000})
+                                        })
+                                        .catch(console.error);
+                                    else
+                                        message.reply(args[2] + ' mensagens foram eliminas!').then(message=>{
+                                             message.delete({timeout:3000})
+                                        })
+                                        .catch(console.error);
+                                }
+                            else
+                                message.reply('Apenas administradores podem eliminar mensagens!');
+                        }
+                    }
+                break;
+            case 'help':
+                if(!args[1])
+                {
+                    const helpEmbed = new Discord.MessageEmbed()
+                    .setColor('#339933')
+                    .setTitle('Menu de Ajuda Rat.exe Bot')
+                    .addFields(
+                        {name: 'Lista de comandos', value: '$help commands'},
+                        {name: '\u200B', value: '\u200B'},
+                        {name: 'Vê o meu código no GitHub!' , value: 'https://github.com/IIIRataxIII/Rat.exe-Bot'}
+                    );
+                    message.channel.send(helpEmbed);
+                }
+                else
+                {
+                    switch(args[1]){
+                        case 'commands':
+                            const helpCommandsEmbed = new Discord.MessageEmbed()
+                            .setColor('#339933')
+                            .setTitle('Lista de Comandos')
+                            .addFields(
+                                {name: '$ping', value: 'O bot respode com pong e se repetires várias vezes ele diz-te o ping dele e o teu, mas ele não gosta muito que repitam várias vezes...'},
+                                {name: '$delete messages <number>', value: 'O bot elimina um certo número de mensagens, apenas admins podem usar este comando.'},
+                                {name: '$abraçar <@alguem>', value: 'O bot dá um abraço a alguém que tu menciones'},
+                                {name: '$bot ping', value: 'Diz o valor do ping do bot'},
+                                {name: '$meu ping', value: 'Diz o valor do teu ping'}
+                            );
+                            message.channel.send(helpCommandsEmbed);
+                            break;
+                        default:
+                            message.reply('Sorry não conheço esse comando, mas se quiseres escreve "$help commands" para veres o que posso fazer.');
+                            break;
                     }
                 }
             break;
-        case 'help':
-            if(!args[1])
-            {
-                const helpEmbed = new Discord.MessageEmbed()
-                .setColor('#339933')
-                .setTitle('Rat.exe Bot Help Menu')
-                .addFields(
-                    {name: 'List of commands', value: '$help commands'},
-                    {name: '\u200B', value: '\u200B'},
-                    {name: 'See my code on GitHub!' , value: 'https://github.com/IIIRataxIII/Rat.exe-Bot'}
-                );
-                message.channel.send(helpEmbed);
-            }
-            else
-            {
-                switch(args[1]){
-                    case 'commands':
-                        const helpCommandsEmbed = new Discord.MessageEmbed()
-                        .setColor('#339933')
-                        .setTitle('Commands List')
-                        .addFields(
-                            {name: '$ping', value: 'Bot replys with Pong, but he doesn\'t like that very much...'},
-                            {name: '$delete messages <number>', value: 'Bot delete a certain number of messages, only admins are allowed to use this command.'}
-                        );
-                        message.channel.send(helpCommandsEmbed);
-                        break;
-                    default:
-                        message.reply('Sorry can\'t find that command if you need help type $help');
-                        break;
-                }
-            }
-        break;
-        default:
-            if(isCommand)
-                message.reply('Sorry can\'t find that command if you need help type $help');
-            break;
+            case 'abraçar':
+                if(!args[1])
+                    return message.reply('Menciona quem queres abraçar, por exemplo "$abraçar @Rat.exe Bot#1386"');
+                message.channel.send('Abraçei o ' + args[1] + ' a pedido do <@!' + message.author.id + '>');
+                break;
+            case 'bot':
+                if(args[1]!='ping')
+                    return message.reply('Será que querias dizer "$bot ping"?');
+                message.channel.send("A testar a conexão...").then(m =>{
+                    var botPing = client.ws.ping;
+                    m.edit(`O meu ping é: ${botPing}ms`);
+                });
+                break;
+            case 'meu':
+                if(args[1]!='ping')
+                    return message.reply('Será que querias dizer "$meu ping"?');
+                message.channel.send("A testar a tua conexão...").then(m =>{
+                    var latency = m.createdTimestamp - message.createdTimestamp;
+                    m.edit(`O teu ping é: ${latency}ms`);
+                });
+                break;
+            default:
+                if(isCommand)
+                    message.reply('Sorry não conheço esse comando, mas se quiseres escreve "$help commands" para veres o que posso fazer.');
+                break;
+        }
     }
 })
 
