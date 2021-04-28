9e65275437d662d4f6073a7afbdc53e2ca54a1fe
IIIRataxIII <56645921+IIIRataxIII@users.noreply.github.com>
2021-04-28 18:06:04 +0100

    Comment all the code
    Every corner of the code has been commented

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-require('dotenv').config()
+require('dotenv').config() //Import the .env library
 
-var commands = require('./commands')
+var commands = require('./commands') //Import the file were all the logic for each command is
 
-const Discord = require('discord.js');
+const Discord = require('discord.js'); //Import the Discord.js library
 
-const client = new Discord.Client();
+const client = new Discord.Client(); //Create a new Discord client
 
-const token = process.env.ACE_BOT_DEV_TOKEN;
+const token = process.env.ACE_BOT_DEV_TOKEN; //Create a variable to keep the token of the bot that is saved on the .env file
 
-var isDevMode, currentBotDiscordId;
+var isDevMode, currentBotDiscordId; //isDevMode - Boolean that is used on the code to know if we are using the dev bot or the real one
+//currentBotDiscordId - Keeps the discord id from the bot
 
-const prefix = '$';
+const prefix = '$'; //Keeps the prefix that the bot is listening. Is static for now...
 
-client.once('ready', ()=> {
-    console.log('Ace Bot is online!');
-    isDevMode = token==process.env.ACE_BOT_DEV_TOKEN;
-    if(isDevMode)
-        currentBotDiscordId = process.env.ACE_BOT_DEV_DISCORD_ID
+client.once('ready', ()=> { //When the bot is ready and online execute this block of code
+    console.log('Ace Bot is online!'); //I think this is quite obvious
+    isDevMode = (token == process.env.ACE_BOT_DEV_TOKEN); // If token is from the dev bot then it isDevMode is true
+    if(isDevMode) //If we are using the dev bot
+        currentBotDiscordId = process.env.ACE_BOT_DEV_DISCORD_ID //The currentBotDiscordId is the dev bot ID
     else
-        currentBotDiscordId = process.env.ACE_BOT_DISCORD_ID
-    client.user.setActivity('$help', {type:'LISTENING'}).catch(console.error);
+        currentBotDiscordId = process.env.ACE_BOT_DISCORD_ID //The currentBotDiscordId is the real bot ID
+    client.user.setActivity('$help', {type:'LISTENING'}).catch(console.error); //Set an activity to the bot saying that he is listening to $help
 });
 
-client.on('message', message=>{
-    
-    let args = message.content;
-    var isCommand = args.charAt(0) == prefix;
-    args= args.substring(prefix.length).split(" ");
-    if(isCommand)
+client.on('message', message=>{ //When the bot identifies a message 
+    let args = message.content; //Keeps the message content
+    var isCommand = args.charAt(0) == prefix; //If the prefix is the one that the bot uses it is a command
+    args= args.substring(prefix.length).split(" "); //Split the command by words and takes out the prefix
+    if(isCommand) //I think this is quite obvious too
     {
-        switch(args[0]){
+        switch(args[0]){ //args[0] is the first word of the command and then depending on that word it seeks waht command to execute from the commands.js file
             case 'ping':
                 commands.ping(message, client);
                 break;
@@ -48,7 +48,7 @@ client.on('message', message=>{
             case 'my':
                 commands.my(args, message, prefix);
                 break;
-            default:
+            default: //If is none of the previous commands
                 if(isCommand)
                     message.reply('Sorry I don\'t recognize that command, but if you want type \"'+ prefix +'help commands\" to see what I can do.');
                 break;
@@ -56,4 +56,4 @@ client.on('message', message=>{
     }
 })
 
-client.login(token);
+client.login(token); //Starts the bot
