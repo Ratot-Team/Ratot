3fde9731375ffb802912cd964c35540ddf78a4d3
Captain Ratax <andrepinto2000@proton.me>
2024-02-23 01:45:45 +0000

    Revert "Issue 102"

--- models/prefixSchema.js
+++ models/prefixSchema.js
@@ -1,17 +1,17 @@
 var mongoose = require("mongoose");
 
 const prefixSchema = new mongoose.Schema(
-	{
-		prefix: "string",
-		guildId: "string",
-		updatedBy: "string",
-	},
-	{ timestamps: true }
+  {
+    prefix: "string",
+    guildId: "string",
+    updatedBy: "string",
+  },
+  { timestamps: true }
 );
 
 const Prefix = mongoose.model("Prefix", prefixSchema, "prefixes");
 
 module.exports = {
-	Prefix,
-	prefixSchema,
+  Prefix,
+  prefixSchema,
 };
